32d03e535618aa8206a666273223624afe2247ce
sakpagare <sakshipagare717@gmail.com>
2024-11-15 15:16:56 +0530

    changes made to payment gateway

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ students = [
     {"name": "John Doe", "prn": "PRN123456"},
     {"name": "Jane Smith", "prn": "PRN654321"},
     {"name": "Alice Johnson", "prn": "PRN987654"},
-    {"name": "Bob Lee", "prn": "PRN456789"}
+    {"name": "Bob Lee", "prn": "PRN456789"},
+    {"name": "Bidakshita","prn":"PRN457690"},
+    {"name": "karan","prn":"PRN5678912"}
 ]
 
 @app.route('/students', methods=['GET'])
